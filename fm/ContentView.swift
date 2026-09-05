7b046a16dbf38e5d1af04c5a74a28ec694e09625
storyofhis <azizi.maula@gmail.com>
2026-09-05 23:20:42 +0700

    Initial Commit

---
+++ fm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fm
+//
+//  Created by Maula Izza Azizi on 05/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fm/fmApp.swift
@@ -0,0 +1,17 @@
+//
+//  fmApp.swift
+//  fm
+//
+//  Created by Maula Izza Azizi on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct fmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
